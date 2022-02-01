1acf4420b61a197bbfedea1a7b07836639375d0e
Darshan Makhecha <65150757+darshan-makhecha@users.noreply.github.com>
2022-02-01 22:05:02 +0530

    New Program Added

---
+++ program/Unit 2/25DeleteDictionary.py
@@ -0,0 +1,13 @@
+my_dict = {'name': 'Jack', 'age': 26}
+#159
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+del dict['Name']; # remove entry with key 'Name'
+print("name Delete after dictionary : ",dict)
+
+dict.clear();     # remove all entries in dict
+print("Dictionary now  clear ",dict)
+del dict ;        # delete entire dictionary
+print("Dictionary is deleted ",dict)
+
---
+++ program/Unit 2/26UpdateDictinary.py
@@ -0,0 +1,8 @@
+# Declare a dictionary
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Age'] = 8; # update existing entry
+print("Before New Record Insert",dict)
+dict['School'] = "DPS School"; # Add new entry
+#print ("dict['Age']: ", dict['Age'])
+#print ("dict['School']: ", dict['School'])
+print("After nsert new Record ", dict)
